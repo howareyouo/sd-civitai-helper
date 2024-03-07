cb5cb2c79af7d2ea915daa9598de6de294c9df6a
hunan <hunan_me@163.com>
2024-03-07 13:04:14 +0800

    style changes

--- scripts/libs/util.py
+++ scripts/libs/util.py
@@ -87,7 +87,7 @@ def shorten_path(filepath: str) -> str:
 # human readable size format
 def hr_size(size, decimal_places=2):
     for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
-        if size < 1024.0 or unit == 'PiB':
+        if size < 1024.0 or unit == 'PB':
             break
         size /= 1024.0
     return f"{size:.{decimal_places}f} {unit}"
